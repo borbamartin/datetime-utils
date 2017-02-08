00ab8f4d15fe7436391d9adbf511e2501107e1d1
Martin B <borbamartin@gmail.com>
2017-02-08 01:04:37 -0500

    Added version constant

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
+VERSION = '1.0.0'
+
 setup(
     name='datetime_utils',
     packages=['datetime_utils'],
-    version='1.0.0',
+    version=VERSION,
     description='A DateTime Utils library',
     author='Martin Borba',
     author_email='borbamartin@gmail.com',
     url='https://github.com/borbamartin/datetime-utils',
-    download_url='https://github.com/borbamartin/datetime-utils/tarball/1.0.0',
+    download_url='https://github.com/borbamartin/datetime-utils/tarball/{}'.format(VERSION),
     keywords=['datetime', 'utils'],
     classifiers=[],
     install_requires=[
